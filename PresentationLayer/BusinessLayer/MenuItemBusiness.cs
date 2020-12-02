c74e6c49892246fe4491b07713eaf30c6deebb31
tomicnikola <50-2018@ftn.edu.rs>
2020-12-02 21:10:17 +0100

    Zavrseni DataAccess i Business sloj

---
+++ PresentationLayer/BusinessLayer/MenuItemBusiness.cs
@@ -0,0 +1,37 @@
+﻿using DataAccesLayer;
+using DataAccesLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class MenuItemBusiness
+    {
+        private readonly MenuItemRepository menuItemRepository;
+
+        public MenuItemBusiness()
+        {
+            this.menuItemRepository = new MenuItemRepository;
+        }
+
+        public List<MenuItem> GetAllMenuItems()
+        {
+            return this.menuItemRepository.GetAllMenuItems();
+        }
+
+        public bool InsertMenuItem(MenuItem mi)
+        {
+            if (this.menuItemRepository.InsertMenuItem(mi) > 0)
+                return true;
+            return false;
+        }
+
+        public List<MenuItem> GetAllMenuItemsIB(decimal min, decimal max)
+        {
+            return this.menuItemRepository.GetAllMenuItems().Where(mi => mi.Price > min && mi.Price < max).ToList();
+        }
+    }
+}
---
+++ PresentationLayer/DataAccesLayer/MenuItemRepository.cs
@@ -0,0 +1,60 @@
+﻿using DataAccesLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer
+{
+    public class MenuItemRepository
+    {
+        public List<MenuItem> GetAllMenuItems()
+        {
+            List<MenuItem> mi = new List<MenuItem>();
+
+            using (SqlConnection con = new SqlConnection(Constants.conString))
+            {
+                SqlCommand com = new SqlCommand();
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM MenuItems";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+
+                while(dr.Read())
+                {
+                    MenuItem item = new MenuItem();
+                    item.Id = dr.GetInt32(0);
+                    item.Title = dr.GetString(1);
+                    item.Description = dr.GetString(2);
+                    item.Price = Convert.ToDecimal(dr.GetDecimal(3));
+
+                    mi.Add(item);
+
+                }
+
+                return mi;
+        
+            }
+        }
+
+        public int InsertMenuItem(MenuItem mi)
+        {
+            int result;
+            using (SqlConnection con = new SqlConnection(Constants.conString))
+            {
+                string commandText = "Insert Into MenuItems(Title, Description, Price) VALUE(@Title, @Description, @Price)";
+                SqlCommand com = new SqlCommand(commandText, con);
+                com.Parameters.AddWithValue("@Title", mi.Title);
+                com.Parameters.AddWithValue("@Description", mi.Description);
+                com.Parameters.AddWithValue("@Price", mi.Price);
+
+                result = com.ExecuteNonQuery();
+            }
+
+            return result;
+        }
+    }
+}
